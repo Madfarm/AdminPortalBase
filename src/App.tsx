2218a24f5f28b514c63c5bb7c9cf01c86a536162
AnthonyScott <anthony.scott1005@gmail.com>
2023-10-25 19:49:19 -0400

    Add login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from 'react-router-dom'
 import './App.css'
 import Home from './Pages/Home'
+import Header from './Components/Header'
 
 function App() {
   return (
     <>
+    <Header />
      <Routes>
         <Route path="/" element={ <Home /> } />
      </Routes>
---
+++ src/Pages/Login.tsx
@@ -0,0 +1,16 @@
+export default function LoginPage() {
+    return (
+        <div className="LoginPage">
+            <h1>Login</h1>
+
+            <form>
+                <label>Username:
+                    <input></input>
+                </label>
+                <label>Password:
+                    <input></input>
+                </label>
+            </form>
+        </div>
+    )
+}
