1da3c38106ccac8d9bc382a46d67c960a45cba5f
AnthonyScott <anthony.scott1005@gmail.com>
2023-10-25 19:54:29 -0400

    Style navbar

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,10 +1,15 @@
 import { Link } from "react-router-dom";
+import './Header.css';
 
 export default function Header() {
     return (
         <nav className="Navbar">
             <Link to="/">Home</Link>
-            <Link to="/login">Login</Link>
+            
+            <div className="AuthLinks">
+                <Link to="/login">Login</Link>
+
+            </div>
         </nav>
     )
 }
