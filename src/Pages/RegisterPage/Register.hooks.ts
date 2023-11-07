2c89021011c7d126a02ad3af5132e18aea0f1df1
AnthonyScott <anthony.scott1005@gmail.com>
2023-11-06 22:07:49 -0500

    Add handle change method for register form inputs

--- src/Pages/RegisterPage/Register.hooks.ts
+++ src/Pages/RegisterPage/Register.hooks.ts
@@ -1,5 +1,18 @@
-import { useState } from "react"
+import { ChangeEvent, ReactHTMLElement, useState } from "react"
 
-export const useRegisterPage = (): void => {
-    const[registerForm, setRegisterForm] = useState<RegisterFormType>();
+export const useRegisterPage = () => {
+    const[registerForm, setRegisterForm] = useState<RegisterFormType>({
+        name: "",
+        email: "",
+        password: ""
+    });
+
+
+
+    var handleChange = (e: React.ChangeEvent<HTMLInputElement>): void => {
+        const { name, value } = e.target;
+        setRegisterForm({...registerForm, [name]: value});
+    } 
+
+    return { handleChange };
 }
